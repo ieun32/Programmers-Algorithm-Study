6b22f9e635b43d857170d6f579631cdea70a88e6
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2023-07-15 00:01:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 196 ms, Memory: 17128 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        // 현재 날짜 구하기 (시스템 시계, 시스템 타임존)
+        LocalDate now = LocalDate.now();
+ 
+        // 결과 출력
+        System.out.println(now);
+    }
+}
