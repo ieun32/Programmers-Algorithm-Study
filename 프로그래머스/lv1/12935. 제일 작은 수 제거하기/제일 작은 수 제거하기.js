4645eaa24a42a252dae6cbe4579124002e9cc9b1
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2023-07-24 23:21:02 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,5 @@
 function solution(arr) {
-    arr.splice(arr.indexOf(arr.reduce((acc, cur)=> Math.min(acc, cur))), 1)
+    arr.splice(arr.indexOf(Math.min(...arr)), 1)
     var answer = arr.length != 0 ? arr : [-1]
     return answer;
 }
