7d8c4d920df3a2a2ad973fd9a347502a6e3fd1d0
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-03-07 17:12:46 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 52.15 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 대소문자 바꿔서 출력하기 - 181949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=javascript) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 45.35 ms
+메모리: 32.3 MB, 시간: 52.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 17:01:00
+2024년 03월 07일 17:12:45
 
 ### 문제 설명
 
--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -10,14 +10,21 @@ rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
     str = input[0];
-    let answer = Array.from(str).map((v, i) => {
-        //아스키코드표 65~90 대문자일 경우
-        if (65 <= v.charCodeAt() && v.charCodeAt() <= 90){
-            return v.toLowerCase()
-        //아스키코드표 97~122 소문자일 경우
-        } else if (97 <= v.charCodeAt() && v.charCodeAt() <= 122) {
-            return v.toUpperCase()
-        }
+    
+    // 첫번째 풀이 (아스키코드)
+    // let answer = [...str].map((v, i) => {
+    //     if (65 <= v.charCodeAt() && v.charCodeAt() <= 90){
+    //         return v.toLowerCase()
+    //     } else if (97 <= v.charCodeAt() && v.charCodeAt() <= 122) {
+    //         return v.toUpperCase()
+    //     }
+    // }).join('')
+    
+    // 두번째 풀이 (toLowerCase(), toUpperCase())
+    let answer = [...str].map((v, i) => {
+        if(v === v.toLowerCase()) return v.toUpperCase()
+        if(v === v.toUpperCase()) return v.toLowerCase()
+        throw new Error('there is no lower, upper string')
     }).join('')
     
     console.log(answer)
