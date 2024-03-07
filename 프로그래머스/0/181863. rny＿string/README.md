422901eb592d9d3b51da12655e86fc925e4bbe98
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-03-07 18:53:57 +0900

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/README.md
+++ 프로그래머스/0/181863. rny＿string/README.md
@@ -1,10 +1,10 @@
 # [level 0] rny_string - 181863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181863) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181863?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 18:52:21
+2024년 03월 07일 18:53:55
 
 ### 문제 설명
 
--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,7 +1,11 @@
 function solution(rny_string) {
-    var arr = [...rny_string]
-    arr.map((v, i) => {
-        if(v === 'm') arr[i] = 'rn'
-    })
-    return arr.join('');
+    // 첫번째 풀이 (순회)
+    // var arr = [...rny_string]
+    // arr.map((v, i) => {
+    //     if(v === 'm') arr[i] = 'rn'
+    // })
+    // return arr.join('');
+    
+    // 두번째 풀이 (내장 메서드)
+    return rny_string.replaceAll('m', 'rn')
 }
