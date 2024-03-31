cb657a17eadd6af83ba8d0d64dbdddeca280b9aa
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-03-31 10:37:22 +0900

    [Silver V] Title: 막대기, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.js
+++ 백준/Silver/1094. 막대기/막대기.js
@@ -1,14 +1,10 @@
 const fs = require('fs').readFileSync('/dev/stdin')
 const X = +fs;
 
-let stick = [32]
+let stick = [64]
 let count = 0;
 
 while(true){
-    if(X === 64){
-        console.log(1)
-        return
-    }
     let sum = stick.reduce((p, c) => p + c)
     if(sum > X){
         let half = stick[stick.length - 1] / 2
