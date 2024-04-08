c26382df4476e4707cefba351f4640f2c54c1c35
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-04-08 21:33:58 +0900

    [Silver III] Title: 주유소, Time: 228 ms, Memory: 21284 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,25 @@
+const fs = require('fs').readFileSync('/dev/stdin')
+const [N, ...input] = fs.toString().trim().split('\n')
+
+let weights = input[0].split(' ').map(Number)
+let prices = input[1].split(' ').map(Number)
+prices = prices.splice(0, prices.length - 1)
+let count = 0;
+
+while (true) {
+  let min = Math.min(...prices)
+  const index = prices.indexOf(min)
+  if (index === 0) {
+    count += min * weights.reduce((p, c) => p + c)
+    break;
+  } else if (index === prices.length - 1){
+    continue;
+  } else {
+    count += min * weights.splice(index, weights.length).reduce((p,c) => p + c)
+    prices = prices.splice(0, index)
+    weights = weights.splice(0, index)
+  }
+}
+
+console.log(count)
+
