4e07143736bb968402a3b69520468ac0586415cc
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-04-08 22:09:25 +0900

    [Silver III] Title: 주유소, Time: 352 ms, Memory: 44604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,23 +1,24 @@
-const fs = require('fs').readFileSync('/dev/stdin')
-const [N, ...input] = fs.toString().trim().split('\n')
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-let weights = input[0].split(' ').map(Number)
-let prices = input[1].split(' ').map(Number)
-prices = prices.splice(0, prices.length - 1)
-let count = 0;
+const N = Number(input.shift());
 
-while (true) {
-  let min = Math.min(...prices)
-  const index = prices.indexOf(min)
-  if (index === 0) {
-    count += min * weights.reduce((p, c) => p + c)
-    break;
-  } else {
-    count += min * weights.splice(index, weights.length).reduce((p,c) => p + c)
-    prices = prices.splice(0, index)
-    weights = weights.splice(0, index)
+const road = input.shift().split(" ").map(BigInt);
+
+const gasPrice = input.shift().split(" ").map(BigInt);
+
+let cost = 0n;
+let currPrice = gasPrice[0];
+
+for (let i = 0; i < N - 1; i++) {
+  cost += currPrice * road[i];
+
+  if (currPrice > gasPrice[i + 1]) {
+    currPrice = gasPrice[i + 1];
   }
 }
 
-console.log(count)
-
+console.log(String(cost));
