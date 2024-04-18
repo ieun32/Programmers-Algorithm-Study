e78547e55f55c268107970f5e05544c0ea71af24
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-04-18 23:30:13 +0900

    [Silver IV] Title: 스택, Time: 200 ms, Memory: 12164 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -1,97 +1,32 @@
-const fs = require('fs').readFileSync('/dev/stdin')
-const [N, ...input] = fs.toString().trim().split('\n')
+const array = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let answer = [];
+const stack = [];
+const result = [];
 
-class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
+const len = array.shift();
+
+for (let i = 0; i < len; i++) {        
+    switch(array[i]) {
+        case 'pop': 
+          result.push(stack.pop() || -1);
+          break;
+
+        case 'size': 
+          result.push(stack.length);
+          break;
+
+        case 'empty': 
+          result.push(stack[0] ? 0 : 1);
+          break;
+
+        case 'top': 
+          result.push(stack[stack.length - 1] || -1);
+          break;
+
+        default: 
+          stack.push(array[i].split(" ")[1]);
+          break;
     }
 }
 
-class Stack {
-    constructor(){
-        let newNode = new Node('head');
-        this.head = newNode;
-        this.length = 0;
-    }
-    
-    push(value){
-        let temp = this.head;
-        let newNode = new Node(value)
-        while(temp.next !== null){
-            temp = temp.next;
-        }
-        temp.next = newNode;
-        this.length++;
-    }
-    
-    pop(){
-        let temp = this.head;
-        let temp2;
-        while(temp.next !== null){
-            temp2 = temp;
-            temp = temp.next;
-        }
-        if(temp.value === 'head'){
-          answer.push(-1)
-          return
-        }
-        answer.push(temp.value)
-        temp2.next = null;
-        this.length--;
-    }
-    
-    empty(){
-        if(this.head.next === null){
-            answer.push(1)
-        } else {
-            answer.push(0)
-        }
-    }
-    
-    top(){
-        let temp = this.head;
-        while(temp.next !== null){
-            temp = temp.next;
-        }
-        if(temp.value === 'head'){
-          answer.push(-1)
-          return
-        }
-        answer.push(temp.value)
-    }
-    
-    size(){
-      answer.push(this.length)
-    }
-}
-
-const stack = new Stack()
-
-
-for(let i = 0; i < input.length; i++){
-    const method = input[i].split(' ')
-
-    switch(method[0]){
-        case 'push':
-            let num = parseInt(method[1])
-            stack.push(num)
-            break;
-        case 'pop':
-            stack.pop()
-            break;
-        case 'size':
-            stack.size()
-            break;
-        case 'empty':
-            stack.empty()
-            break;
-        case 'top':
-            stack.top()
-            break;
-    }
-}
-
-console.log(answer.join('\n'))
+console.log(result.join('\n'));
