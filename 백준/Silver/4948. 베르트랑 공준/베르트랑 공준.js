3c7cd745ffe25a40dce24651f860b7251fcff271
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-04-24 18:20:55 +0900

    [Silver II] Title: 베르트랑 공준, Time: 256 ms, Memory: 16520 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
@@ -1,28 +1,33 @@
-const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(v => +v);
 
-let TC = 0;
 
-function sol() {
-  let answer = '';
-  
-  while (1) {
-    let n = +input[TC++];
-    if (n === 0) break; // 입력값에 0이 있으면 반복문을 종료하면 된다. 마지막값이라 
+const max = Math.max(...input) * 2;
 
+let prime = [];
+for (let i = 0; i <= max; i++) {
+  prime.push(true);
+}
+prime[0] = false;
+prime[1] = false;
+
+
+for (let i = 2; i * i <= max; i++) {
+  if (prime[i])
+    for (let j = i * i; j <= max; j += i)
+      prime[j] = false;
+}
+
+input.forEach(v => {
+  const start = v;
+  const end = v * 2;
+  if (v > 0) {
     let cnt = 0;
-    for (let i = n + 1; i <= 2 * n; i++) { //N부터 2N까지 소수가 몇 개 인지 세기
-      if (isPrime(i)) cnt++;
+    for (let i = start + 1; i <= end; i++) {
+      if (prime[i] == true) {
+        cnt++;
+      }
     }
-    answer += `${cnt}\n`;
+    console.log(cnt)
   }
-  return answer;
-}
-
-function isPrime(n) {
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
-
-console.log(sol().trim());
+})
