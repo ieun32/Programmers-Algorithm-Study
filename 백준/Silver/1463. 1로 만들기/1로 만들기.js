c4db9bd5a456ebabd988cd582864e3a5bb330298
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-05-07 14:43:39 +0900

    [Silver III] Title: 1로 만들기, Time: 192 ms, Memory: 17432 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,24 +1,15 @@
-const input = +require("fs").readFileSync("/dev/stdin").toString();
+const N = +require("fs").readFileSync("/dev/stdin")
 
-const dp = new Array(input + 1);
-
+const dp = Array(N + 1).fill(0);
 dp[1] = 0;
-dp[2] = dp[1] + 1;
-dp[3] = Math.min(dp[1] + 1, dp[2] + 1);
+dp[2] = 1;
+dp[3] = 1;
 
-for (let i = 4; i <= input; i++) {
-  if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
-  }
-  if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
-  }
-  if (i % 2 === 0 && i % 3 === 0) {
-    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1, dp[i / 3] + 1);
-  }
-  if (i % 2 !== 0 && i % 3 !== 0) {
-    dp[i] = dp[i - 1] + 1;
-  }
+for(let i = 4; i <= N; i++){
+    const divideTwo = i % 2 === 0 ? dp[i / 2] + 1 : Infinity;
+    const divideThree = i % 3 === 0 ? dp[i / 3] + 1 : Infinity;
+    const minusOne = dp[i - 1] + 1;
+    dp[i] = Math.min(divideTwo, divideThree, minusOne);
 }
 
-console.log(dp[input]);
+console.log(dp[N])
