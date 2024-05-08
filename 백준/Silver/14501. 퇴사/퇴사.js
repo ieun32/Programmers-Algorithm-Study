f4a3ae416a896ac2f2721d912c6fbb37a7865759
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-05-08 11:09:05 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -1,20 +1,16 @@
-const [n, ...arr] = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-const N = Number(n);
-const counsel = arr.map(tp => tp.split(' ').map(Number));
- 
-function solution(n, counsel){
-    const DP = new Array(N).fill(0);
-    
-    for (let i = 0; i < n; i++) {
-        const [period, profit] = counsel[i];
-        if (i + period > N) continue;
-        DP[i] = DP[i] + profit;
-         
-        for (let j = i + period; j < N; j++) {
-            DP[j] = Math.max(DP[j], DP[i]);
-        }
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n')
+const N = +input.shift();
+const T = input.map((info) => info.split(' ').map(Number)[0]);
+const P = input.map((info) => info.split(' ').map(Number)[1]);
+
+const dp = new Array(N + 1).fill(0);
+for(let i = N - 1; i >= 0; i--){
+    if(i + T[i] > N){
+        dp[i] = dp[i + 1];
+        continue;
     }
-    return Math.max(...DP);
+  
+  dp[i] = Math.max(P[i] + dp[i + T[i]], dp[i + 1]);
 }
- 
-console.log(solution(N, counsel));
+
+console.log(dp[0])
