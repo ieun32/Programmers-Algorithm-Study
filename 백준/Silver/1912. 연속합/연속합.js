cf99e5ea1b717aee41360cc8dd8655d7df48e971
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-05-19 19:15:33 +0900

    [Silver II] Title: 연속합, Time: 200 ms, Memory: 16584 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.js
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -1,9 +1,14 @@
-var fs = require('fs');
-var inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
-var cases = Number(inputs[0]);
-inputs = inputs[1].split(' ').map(v=>Number(v));
-var dp = [inputs[0]];
-for(var i=1; i<cases; i++){
-    dp[i] = inputs[i] > inputs[i] + dp[i-1] ? inputs[i] : inputs[i] + dp[i-1];
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const N = +input.shift();
+const inputs = input[0].split(" ").map(Number);
+
+// dp[N] = max(누적값+현재선택, 이전값+현재선택, 현재선택)
+// 해서 dp중에 가장 큰 값을 출력하면 될까?
+const dp = [];
+dp[0] = inputs[0];
+
+for(let i = 1; i < inputs.length; i++){
+    dp[i] = Math.max(dp[i - 1] + inputs[i], inputs[i - 1] + inputs[i], inputs[i]);
 }
+
 console.log(Math.max(...dp));
