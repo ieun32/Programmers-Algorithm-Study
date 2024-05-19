3475c614e504f019e0d94d163e27bec378cfb1dc
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-05-19 19:49:05 +0900

    [Silver II] Title: 연속합, Time: 184 ms, Memory: 26476 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.js
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -5,10 +5,15 @@ const inputs = input[0].split(" ").map(Number);
 // dp[N] = max(누적값+현재선택, 이전값+현재선택, 현재선택)
 // 해서 dp중에 가장 큰 값을 출력하면 될까?
 const dp = [];
-dp[0] = inputs[0];
 
-for(let i = 1; i < inputs.length; i++){
-    dp[i] = Math.max(dp[i - 1] + inputs[i], inputs[i - 1] + inputs[i], inputs[i]);
+function TopDown(num) {
+  if (num === 0) {
+    dp[num] = inputs[num];
+    return dp[num];
+  }
+  dp[num] = Math.max(TopDown(num - 1) + inputs[num], inputs[num]);
+  return dp[num];
 }
 
-console.log(Math.max(...dp));
+TopDown(N - 1);
+console.log(Math.max(...dp))
