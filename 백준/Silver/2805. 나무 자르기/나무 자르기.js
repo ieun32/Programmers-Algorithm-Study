376fd55edef95239e038136d3461ac98ff69a46a
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-05-20 13:06:20 +0900

    [Silver II] Title: 나무 자르기, Time: 644 ms, Memory: 143056 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -1,30 +1,26 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const [N, M] = input.shift().split(' ');
-const trees = input[0].split(' ').map(Number).sort((a,b) => a-b);
-
-function solve(arr, target) {
-    let start = 0;
-    let end = arr[arr.length-1];
-    let answer = Number.MIN_SAFE_INTEGER;
-    while(start <= end) {
-        let mid = Math.floor((start + end)/2);
-        let sum = 0;
-        for(let x of arr) {
-            if(x > mid) sum += x-mid;
-        }
-
-        if(sum >= target) {
-            if(mid > answer) answer = mid; 
-            // 최댓값 계속 구해주기.
-          	// 이 부분을 제외하고는 일반적인 이분탐색 코드와 똑같다.
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, M] = input.shift().split(" ").map(Number);
+const Trees = input[0].split(" ").map(Number);
+let [left, mid, right] = [0, 0, Math.max(...Trees)];
+mid = Math.floor((left + right) / 2)
+let sum = 0;
+while (true) {
+  sum = 0;
+  for (let i = 0; i < N; i++) {
+    if (Trees[i] - mid > 0) {
+      sum += Trees[i] - mid;
     }
-
-    return answer;
+  }
+  if (sum === M || left === mid || mid === right) {
+    console.log(mid)
+    return
+  }
+  if (M < sum) {
+    left = mid;
+    mid = Math.floor((left + right) / 2);
+  }
+  if (M > sum) {
+    right = mid;
+    mid = Math.floor((left + right) / 2);
+  }
 }
-
-console.log(solve(trees, M))
