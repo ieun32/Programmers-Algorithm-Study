cb8cb37ff9c6e4ae622ef45fe7edf12051a75e73
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-06-10 13:01:31 +0900

    [Silver I] Title: 스티커, Time: 444 ms, Memory: 69616 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.js
+++ 백준/Silver/9465. 스티커/스티커.js
@@ -1,25 +1,28 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 const T = +input.shift();
-const answer = [];
 
-for (let i = 0; i < input.length;) {
+// dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + top[i];
+// dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + bottom[i];
+// answer = Math.max(dp[0][N - 1], dp[1][N - 1]);
+const answer = [];
+for (let i = 0; i < T * 3;) {
   const N = +input[i];
-  const array = [];
-  for (let j = 1; j <= 2; j++) {
-    array.push(input[i + j].split(" ").map(Number))
+  const top = input[i + 1].split(" ").map(Number);
+  const bottom = input[i + 2].split(" ").map(Number);
+  const dp = Array.from({ length: 2 }, () => Array(N).fill(0));
+
+  dp[0][0] = top[0];
+  dp[1][0] = bottom[0];
+  dp[0][1] = bottom[0] + top[1];
+  dp[1][1] = top[0] + bottom[1];
+
+  for (let i = 2; i < N; i++) {
+    dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + top[i];
+    dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + bottom[i];
   }
-  let dp = [];
-  for (let i = 0; i <= N; i++) {
-    dp.push([0, 0]);
-  }
-  dp[1][0] = array[0][0];
-  dp[1][1] = array[1][0];
-  for (let i = 2; i <= N; i++) {
-    dp[i][0] = Math.max(dp[i - 1][1], dp[i - 2][1]) + array[0][i - 1] * 1;
-    dp[i][1] = Math.max(dp[i - 1][0], dp[i - 2][0]) + array[1][i - 1] * 1;
-  }
-  answer.push(Math.max(...dp[N]));
-  i = i + 3;
+
+  answer.push(Math.max(dp[0][N - 1], dp[1][N - 1]));
+  i += 3;
 }
 
-console.log(answer.join("\n"))
+console.log(answer.join("\n"));
