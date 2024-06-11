ce9201229eb2bd31128eb0d4780d6d796f77e3db
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-06-11 22:46:43 +0900

    [Gold V] Title: 토마토, Time: 692 ms, Memory: 169420 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.js
+++ 백준/Gold/7569. 토마토/토마토.js
@@ -1,50 +1,49 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const [M, N, H] = input.shift().split(' ').map(Number);
-const dx = [-1, 1, 0, 0, 0, 0];  // 좌,우,상,하,앞,뒤일 때 x좌표
-const dy = [0, 0, -1, 1, 0, 0]; // 좌,우,상,하,앞,뒤일 때 y좌표
-const dz = [0, 0, 0, 0, -1, 1]; // 좌,우,상,하,앞,뒤일 때 z좌표 (상자 인덱스)
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const [M, N, H] = input.shift().split(" ").map(Number);
+
+// 이동 좌표
+const dn = [-1, 0, 1, 0, 0, 0];
+const dm = [0, -1, 0, 1, 0, 0];
+const dh = [0, 0, 0, 0, -1, 1];
+
+// 초기화
+const boxes = Array.from(Array(H), () => Array.from(Array(N), () => Array(M).fill(0)));
+for (let h = 0; h < H; h++) {
+  for (let n = 0; n < N; n++) {
+    boxes[h][n] = input.shift().split(" ").map(Number);
+  }
+}
+
+// 시작 좌표들 초기화
 const queue = [];
-const boxes = Array.from(Array(H), () => Array.from(Array(N), () => Array.from(Array(M).fill(0))));
-
-// 3차원 배열에 입력값 삽입
-for (let i = 0; i < H; i++) {
-  for (let j = 0; j < N; j++) {
-    boxes[i][j] = input.shift().split(' ').map(Number);
-  }
-}
-
-let unripeTomato = 0;
-for (let i = 0; i < H; i++) {
-  for (let j = 0; j < N; j++) {
-    for (let k = 0; k < M; k++) {
-			// 익지 않은 토마토이면 카운트 증가
-      if (boxes[i][j][k] == 0) unripeTomato++;
-			// 익은 토마토이면 큐에 담기 (현재 위치 및 걸린 일수 초기값 0)
-      if (boxes[i][j][k] == 1) queue.push([i, j, k, 0]);
+let unripe = 0;
+for (let h = 0; h < H; h++) {
+  for (let n = 0; n < N; n++) {
+    for (let m = 0; m < M; m++) {
+      if (boxes[h][n][m] === 1) {
+        queue.push([h, n, m, 0]); // 높이, 세로, 가로, 탐색 깊이
+      }
+      if (boxes[h][n][m] === 0) {
+        unripe++;
+      }
     }
   }
 }
 
-let idx = 0; // 큐 인덱스
-let answer = 0; // 걸린 기간(일수) 담을 변수
-while (queue.length > idx) {
-  const [z, x, y, days] = queue[idx++];
-
-	// 현재 위치 기준 인접한 여섯 곳 탐색할 반복문
+let depth = 0;
+let index = 0;
+while (index < queue.length) {
+  const [h, n, m, d] = queue[index++];
   for (let i = 0; i < 6; i++) {
-    const nx = x + dx[i];
-    const ny = y + dy[i];
-    const nz = z + dz[i];
-
-		// 해당 위치가 상자(그래프) 범위 밖으로 벗어나지 않았고, 안 익은 토마토이면
-    if (nz >= 0 && nz < H && nx >= 0 && nx < N && ny >= 0 && ny < M && !boxes[nz][nx][ny]) {
-      boxes[nz][nx][ny] = 1; // 토마토 익힘
-      queue.push([nz, nx, ny, days + 1]); // 큐에 해당 위치와 하루 증가한 값 담기
-      unripeTomato--; // 안 익은 토마토 개수 감소
+    const [nH, nN, nM] = [h + dh[i], n + dn[i], m + dm[i]];
+    if (nH < 0 || nN < 0 || nM < 0 || nH >= H || nN >= N || nM >= M) continue;
+    if (boxes[nH][nN][nM] === 0) {
+      boxes[nH][nN][nM] = 1;
+      queue.push([nH, nN, nM, d + 1]);
+      unripe--;
+      depth = d + 1;
     }
   }
-  answer = days;
 }
 
-// 안 익은 토마토가 있다면 -1 반환, 없으면 걸린 기간(일수) 반환 
-console.log(unripeTomato ? -1 : answer);
+console.log(unripe === 0 ? depth : -1);
