50d3381cb6d47ba1c4626bd798f0743d90be6cf1
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-06-11 23:33:06 +0900

    [Gold V] Title: 용액, Time: 208 ms, Memory: 25892 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.js
+++ 백준/Gold/2467. 용액/용액.js
@@ -1,14 +1,32 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-const N = +input[0]
-const A = input[1].split(' ').map(Number)
-let [i, j, min, output] = [0, N - 1, Infinity, '']
-while (i < j) {
-  const sum = A[i] + A[j]
-  if (Math.abs(sum) < min) {
-    min = Math.abs(sum)
-    output = `${A[i]} ${A[j]}`
-  }
-  sum < 0 ? i++ : j--
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const N = +input.shift();
+const solutions = input[0].split(" ").map(Number);
+
+function findSolution(){
+    let p1 = 0;
+    let p2 = solutions.length - 1;
+    let min = Number.MAX_SAFE_INTEGER;
+    let answer = [];
+    while(p1 < p2){
+        const left = solutions[p1]
+        const right = solutions[p2]
+        
+        if(Math.abs(left + right) < min){
+            answer = [left, right];
+            min = Math.abs(left + right);
+        }
+        if(left + right < 0){
+            p1 = p1 + 1;
+        }
+        if(left + right > 0){
+            p2 = p2 - 1;
+        }
+        if(left + right === 0){
+            console.log(answer.join(" "))
+            return
+        }
+    }
+    console.log(answer.join(" "))
 }
-console.log(output)
+
+findSolution();
