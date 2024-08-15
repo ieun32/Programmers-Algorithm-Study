a9e8bb4fe2ef2d73fed54e454ebde46da8ba4cda
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2024-08-15 21:25:25 +0900

    [Silver I] Title: 미로 탐색, Time: 172 ms, Memory: 12340 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,45 +1,37 @@
-const fs = require("fs").readFileSync("/dev/stdin");
-const [NM, ...input] = fs.toString().trim().split("\n");
-const [N, M] = NM.split(" ").map(Number);
-const map = input.map((row) => row.split("").map(Number));
+const fs = require('fs').readFileSync('/dev/stdin');
+const [NM, ...rows] = fs.toString().trim().split('\n');
+const [N, M] = NM.split(' ').map(Number); // 세로 N, 가로 M
+const maze = rows.map((row) => row.split('').map(Number));
 
-// 상하좌우 이동좌표
-const dx = [0, 0, -1, 1];
-const dy = [1, -1, 0, 0];
+const dx = [1, 0, -1, 0];
+const dy = [0, -1, 0, 1];
 
 function BFS() {
-  let queue = [[0, 0]]; // (1,1)에서 탐색 시작
-  let visited = Array.from({ length: N }, () => new Array(M).fill(false)) // 방문 배열
-  let count = 1;
+  const queue = [[0, 0, 1]];
+  const visited = Array.from({ length: N }, () => new Array(M).fill(false));
 
-  while (true) {
-    if (queue.length === 0) {
-      console.log(count);
-      break;
+  while (queue.length) {
+    const [x, y, d] = queue.shift();
+    visited[x][y] = true;
+
+    // [N, M]에 도착한 경우 d 반환
+    if (x === N - 1 && y === M - 1) {
+      return d;
     }
-    const length = queue.length;
-    for (let j = 0; j < length; j++) {
-      const [x, y] = queue.shift();
-      visited[x][y] = true;
-      map[x][y] = 0;
-      if (x === N - 1 && y === M - 1) {
-        console.log(count);
-        return;
-      }
-      for (let i = 0; i < 4; i++) {
-        const X = x + dx[i];
-        const Y = y + dy[i];
 
-        if (X < 0 || Y < 0 || X >= N || Y >= M) continue
-        if (!visited[X][Y] && map[X][Y] === 1) {
-          queue.push([X, Y]);
-          visited[X][Y] = true;
-          map[X][Y] = 0;
-        }
+    // 도착하지 않은 경우 이동할 수 있는 좌표 모두 queue에 삽입
+    for (let i = 0; i < 4; i++) {
+      const X = x + dx[i];
+      const Y = y + dy[i];
+      const D = d + 1;
+
+      if (X < 0 || Y < 0 || X >= N || Y >= M) continue;
+      if (!visited[X][Y] && maze[X][Y] !== 0) {
+        queue.push([X, Y, D]);
+        visited[X][Y] = true;
       }
     }
-    count++;
   }
 }
 
-BFS();
+console.log(BFS());
