bead18206e7918cb380b98df269b18afa6d322f8
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2025-04-18 03:22:03 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,12 +1,21 @@
-const fs = require('fs').readFileSync('/dev/stdin')
-const [input, ...coins] = fs.toString().trim().split('\n')
-let [N, K] = input.split(' ').map(Number)
+const fs = require('fs').readFileSync('/dev/stdin');
+const [input, ...coins] = fs.toString().trim().split('\n');
+let [N, K] = input.split(' ').map(Number);
 let count = 0;
-for(let i = N; i > 0; i--){
-    if(K / coins[i - 1] >= 1){
-        count += Math.trunc(K / coins[i - 1])
+
+// coins 배열을 숫자 배열로 변환
+const coinValues = coins.map(Number);
+
+let index = coinValues.length - 1;
+
+while (K > 0 && index >= 0) {
+    const coin = coinValues[index];
+    if (coin <= K) {
+        // 현재 동전으로 가능한 개수 계산
+        count += Math.floor(K / coin);
+        K %= coin;
     }
-    K = K % coins[i - 1];
+    index--;
 }
 
-console.log(count)
+console.info(count);
