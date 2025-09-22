e81db320e4df72a45c25dfbc6228f4d33ea53611
Eunyoung Lee <116702472+ieun32@users.noreply.github.com>
2025-09-22 19:01:49 +0900

    [Silver V] Title: 돌 게임, Time: 152 ms, Memory: 9384 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.js
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -1,8 +1,12 @@
-const fs = require('fs').readFileSync('/dev/stdin')
-const input = +fs;
+const fs = require('fs').readFileSync('/dev/stdin');
+const N = Number(fs.toString());
+// 2개를 남겨야 이김
+// 3으로 나눈 몫이 홀수 & 나머지가 짝수
+// 3으로 나눈 몫이 짝수 & 나머지가 홀수면 상근이가 이김
+// 그니까 3으로 나눈 몫 + 나머지 = 홀수면 상근이가 이김
 
-if(input % 2 === 0){
-    console.log('CY')
-} else {
+if((Math.floor(N / 3) + N % 3) % 2 !== 0){
     console.log('SK')
-}
+} else {
+    console.log('CY')
+};
